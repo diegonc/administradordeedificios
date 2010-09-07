296eb29d2d7bb63181014cfc4640b44be5224650
dmpstaltari <dmpstaltari@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-07 04:02:31 +0000

    --Agrego DTO y archivos de configuracion de hibernate

---
+++ ejemplo/src/expensas/ExpensaLiquidacionDTO.java
@@ -0,0 +1,57 @@
+package expensas;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="EXPENSA_LIQUIDACION")
+public class ExpensaLiquidacionDTO implements Serializable{
+
+	private int id;
+	private double deudaPrevia;
+	private double interesSegundoVencimiento;
+	private ExpensaDTO expensa;
+	private int version;
+	
+	@Id @Column(name="ID")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(name="DEUDA_PREVIA",nullable=true)
+	public double getDeudaPrevia() {
+		return deudaPrevia;
+	}
+	public void setDeudaPrevia(double deudaPrevia) {
+		this.deudaPrevia = deudaPrevia;
+	}
+	
+	@Column(name="INT_SEGUNDO_VTO",nullable=true)
+	public double getInteresSegundoVencimiento() {
+		return interesSegundoVencimiento;
+	}
+	public void setInteresSegundoVencimiento(double interesSegundoVencimiento) {
+		this.interesSegundoVencimiento = interesSegundoVencimiento;
+	}
+	
+	//Mapear con ExpensaDTO	
+	public ExpensaDTO getExpensa() {
+		return expensa;
+	}
+	public void setExpensa(ExpensaDTO expensa) {
+		this.expensa = expensa;
+	}
+	@Version @Column(name="VERSION")
+	public int getVersion() {
+		return version;
+	}
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+}
---
+++ ejemplo/src/propiedades/PropiedadDTO.java
@@ -0,0 +1,12 @@
+package propiedades;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="PROPIEDAD")
+public class PropiedadDTO implements Serializable{
+
+}
