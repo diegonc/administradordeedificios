a04bb439585e95e437b3b5e010bdf881a72c30a0
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-10 03:08:05 +0000

    agrego package donde se encuentran los actions

---
+++ ejemplo/src/actions/LoginAction.java
@@ -0,0 +1,41 @@
+package actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+@SuppressWarnings("serial")   
+public class LoginAction extends ActionSupport {
+	
+    private String username;
+    
+    private String password;
+  
+    public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {   
+        return username;   
+    }   
+  
+    public void setUsername(String nombre) {   
+        this.username = nombre;   
+    }   
+    
+  
+    public String execute() {   
+        System.out.println(username);   
+        
+        if (this.password.equals(this.username))
+        {
+        	return SUCCESS;
+        
+        }else
+        	return "error";
+           
+    }   
+}  
