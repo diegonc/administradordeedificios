e3cf25671a96f6eff4a3d6ccdee3586a26d85304
tanomartin05@gmail.com <tanomartin05@gmail.com@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-10 21:17:21 +0000

    OJO... hibernate.cfg.xml le cambio el pass yo tengo root como password. Esta el TestEdificio.java.. que es como el Test de factura que mando dario Cree el paquete test, ahi ya cree el TestEdificio y cree una prueba. Despues sigo agregando mas pruebas asi hacemos todas en base a estas.

---
+++ trunk/ejemplo/src/edificio/EdificioTransacBD.java
@@ -0,0 +1,144 @@
+package edificio;
+import org.hibernate.*;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.criterion.Expression;
+
+import org.apache.log4j.Logger;
+
+import edificio.EdificioDTO;
+
+import java.util.List;
+
+/**
+ * Manipulación de una tabla simple con llave sencilla.
+ */
+public final class EdificioTransacBD {
+	private static Logger _logger = Logger.getLogger(EdificioTransacBD.class);
+
+	/**
+	 * Obtener un SessionFactory de un archivo de configuración local.
+	 */
+	public SessionFactory createSessionFactory() {
+		AnnotationConfiguration configuration = new AnnotationConfiguration();
+		configuration.configure("/hibernate.cfg.xml");
+		return configuration.buildSessionFactory();
+	}
+
+	public int insertObject(Session session, EdificioDTO edificio) {
+		boolean commit = false;
+		Transaction transaction = session.beginTransaction();
+		try {
+			session.save(edificio);
+			commit = true;
+		} finally {
+			if (commit) {
+				transaction.commit();
+			} else {
+				transaction.rollback();
+			}
+		}
+		return edificio.getId();
+	}
+
+	/**
+	 * Perform SELECT statements using Hibernate's load() method
+	 */
+	public EdificioDTO getObject(SessionFactory factory, int id) {
+		Session session = factory.openSession();
+		try {
+			return (EdificioDTO) session.load(EdificioDTO.class, id);
+		} finally {
+			session.close();
+		}
+	}
+
+	/**
+	 * Perform SELECT statements using Hibernate's Criteria API
+	 */
+	public EdificioDTO queryObject(SessionFactory factory, int id) {
+		Session session = factory.openSession();
+		try {
+			Criteria criteria = session.createCriteria(EdificioDTO.class);
+			criteria.add(Expression.eq("id", id));
+			List<?> results = criteria.list();
+			return (EdificioDTO) results.get(0);
+		} finally {
+			session.close();
+		}
+	}
+
+	/**
+	 * Perform SELECT statements using Hibernate's Query API
+	 */
+	public int findMaxKey(SessionFactory factory) {
+		Session session = factory.openSession();
+		try {
+			Query query = session
+					.createQuery("SELECT MAX( edificio.id ) FROM EdificioDTO edificio");			
+			List<?> results = query.list();
+			return (Integer) results.get(0);
+		} finally {
+			session.close();
+		}
+	}
+
+	/**
+	 * Perform UPDATE statements using Hibernate's update() method
+	 */
+	public void updateObject(SessionFactory factory, EdificioDTO edificio) {
+		Session session = factory.openSession();
+		try {
+			updateObject(session, edificio);
+		} finally {
+			session.close();
+		}
+	}
+
+	public void updateObject(Session session, EdificioDTO edificio) {
+		boolean commit = false;
+		Transaction transaction = session.beginTransaction();
+		try {
+			session.update(edificio);
+			commit = true;
+		} finally {
+			if (commit) {
+				transaction.commit();
+			} else {
+				transaction.rollback();
+			}
+		}
+	}
+
+	/**
+	 * Perform DELETE statements using Hibernate's Query API
+	 */
+	public void deleteObject(SessionFactory factory, int id) {
+		Session session = factory.openSession();
+		try {
+			deleteObject(session, id);
+		} finally {
+			session.close();
+		}
+	}
+
+	public void deleteObject(Session session, Integer pk) {
+		boolean commit = false;
+		Transaction transaction = session.beginTransaction();
+
+		try {
+			Query query = session
+					.createQuery("DELETE FROM EdificioDTO factura WHERE factura.id = :id");
+			query.setInteger("id", pk);
+			query.executeUpdate();
+			commit = true;
+		} finally {
+			if (commit) {
+				transaction.commit();
+			}
+			else {
+				transaction.rollback();
+			}
+		}
+	}
+
+}
