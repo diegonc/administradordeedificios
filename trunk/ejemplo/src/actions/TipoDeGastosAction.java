157f6171ff635a8f8bef7f11fe33556208507479
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-10 03:08:05 +0000

    agrego package donde se encuentran los actions

---
+++ trunk/ejemplo/src/actions/TipoDeGastosAction.java
@@ -0,0 +1,33 @@
+package actions;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class TipoDeGastosAction extends ActionSupport {
+	
+	private String codigo;
+	
+	private String descripcion;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	
+	public String execute() {
+		return "success";
+	}
+	
+
+}
