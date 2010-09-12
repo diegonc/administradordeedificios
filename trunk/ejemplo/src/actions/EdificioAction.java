a4dcc8987cd8611488bb53580a2262bdc61fa417
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-12 00:20:54 +0000

    agrego el action para edificios

---
+++ trunk/ejemplo/src/actions/EdificioAction.java
@@ -0,0 +1,13 @@
+package actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class EdificioAction extends ActionSupport {
+	
+	public String excecute(){
+		return SUCCESS;
+		
+	}
+	
+
+}
