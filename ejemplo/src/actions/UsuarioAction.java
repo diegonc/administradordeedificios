9501c764f98b1476df1bd27fb202c4f7401b222a
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-14 02:35:51 +0000

    vista de listado para usuario provisoria

--- ejemplo/src/actions/UsuarioAction.java
+++ ejemplo/src/actions/UsuarioAction.java
@@ -1,10 +1,19 @@
 package actions;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Vector;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import usuarios.dto.UsuarioDTO;
 
+
+import beans.UsuariosBean;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 
@@ -46,11 +55,38 @@ public class UsuarioAction extends ActionSupport implements SessionAware {
 	
 	public String execute() {
 		
-    	System.out.println(this.nombre);
-    	System.out.println(this.apellido);
-    	System.out.println(this.dni);
-    	System.out.println(this.contrasena);
-     return "success";
+    	UsuarioDTO user1 = new UsuarioDTO();
+    	UsuarioDTO user2 = new UsuarioDTO();
+    	
+    	user1.setUsuario("aChelotti");
+    	user1.setPerfil(0);
+    	user1.setPassword("aChelotti");
+    	user1.setNombre("Adriana");
+    	user1.setApellido("Chelotti");
+    	user1.setDni(31026053);
+    	
+    	
+    	
+    	user2.setUsuario("dStaltari");
+    	user2.setPerfil(0);
+    	user2.setPassword("dStaltari");
+    	user2.setNombre("Dario");
+    	user2.setApellido("Staltari");    	
+    	user2.setDni(30672871);
+    	
+    	LinkedList<UsuarioDTO> lista = new LinkedList<UsuarioDTO>();
+    	lista.add(user1);
+    	lista.add(user2);
+    	UsuariosBean listaUsuarios = new UsuariosBean();
+    	listaUsuarios.setUsers(lista);
+    	
+    	Map session = ActionContext.getContext().getSession();
+         session.put("lista",listaUsuarios);
+        this.setSession(session);
+         
+                       
+
+    	 return "success";
     }
 	public void setSession(Map<String, Object> arg0) {
 		// TODO Auto-generated method stub
