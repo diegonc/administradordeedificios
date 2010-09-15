037fe656927f1ed685cca1fd1920c0fefb7f51ea
dmpstaltari <dmpstaltari@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-15 08:53:21 +0000

    Cambio en las notaciones de hibernate para manejo de herencia

--- trunk/ejemplo/src/expensas/ExpensaLiquidacionDTO.java
+++ trunk/ejemplo/src/expensas/ExpensaLiquidacionDTO.java
@@ -1,34 +1,18 @@
 package expensas;
 
-import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
-import javax.persistence.*;
-
 @SuppressWarnings("serial")
 @Entity
 @Table(name="EXPENSA_LIQUIDACION")
-public class ExpensaLiquidacionDTO implements Serializable{
 @PrimaryKeyJoinColumn(name="EXPENSA_ID")
 public class ExpensaLiquidacionDTO extends ExpensaDTO{
 
-	private int id;
 	private double deudaPrevia;
 	private double interesSegundoVencimiento;
-	private ExpensaDTO expensa;
-	private int version;
-	
-	@Id @Column(name="ID")
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
 		
 		
 	@Column(name="DEUDA_PREVIA",nullable=true)
@@ -46,21 +30,5 @@ public class ExpensaLiquidacionDTO extends ExpensaDTO{
 	public void setInteresSegundoVencimiento(double interesSegundoVencimiento) {
 		this.interesSegundoVencimiento = interesSegundoVencimiento;
 	}
-	
-	//Mapear con ExpensaDTO	
-	public ExpensaDTO getExpensa() {
-		return expensa;
-	}
-	public void setExpensa(ExpensaDTO expensa) {
-		this.expensa = expensa;
-	}
-	@Version @Column(name="VERSION")
-	public int getVersion() {
-		return version;
-	}
-	public void setVersion(int version) {
-		this.version = version;
-	}
-	
 		
 }
