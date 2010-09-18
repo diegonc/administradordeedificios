0446597dcb9f83bca8b6daa3c50bfc7f30ca7695
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-18 21:33:02 +0000

    Action para el listado de usuarios

---
+++ ejemplo/src/actions/GetListadoUsuariosAction.java
@@ -0,0 +1,50 @@
+package actions;
+
+
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import usuarios.appl.UsuarioAppl;
+import usuarios.dto.UsuarioDTO;
+import beans.UsuariosBean;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GetListadoUsuariosAction extends ActionSupport implements SessionAware{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, Object> session;
+
+	public String execute() {
+		UsuarioAppl usuarioAppl = new UsuarioAppl();
+		
+		List<UsuarioDTO> listaUsuario = usuarioAppl.getUsuarios() ;
+		
+		
+		
+		UsuariosBean listado = new UsuariosBean();
+		listado.setUsers(listaUsuario);
+		
+		Map session = ActionContext.getContext().getSession();
+        session.put("listado",listado);
+        this.setSession(session);
+         
+                    
+
+    	 return "success";
+    }
+
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		this.session=arg0;
+		
+	}
+}
