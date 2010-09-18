c25f79cf7e33c54c3b3d9a91e6737076ce9950dd
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-18 21:38:03 +0000

    Bean para cargar los perfiles de los usuarios

---
+++ trunk/ejemplo/src/beans/PerfilesBean.java
@@ -0,0 +1,22 @@
+package beans;
+
+
+import java.util.List;
+
+
+import usuarios.dto.PerfilDTO;
+
+public class PerfilesBean {
+	
+	private List<PerfilDTO> perfiles;
+
+	public List<PerfilDTO> getPerfiles() {
+		return perfiles;
+	}
+
+	public void setPerfiles(List<PerfilDTO> list) {
+		this.perfiles = list;
+	}
+	
+
+}
