2cc603caf1bc64e294f35640f8491cfe7c93e5f1
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-19 18:48:28 +0000

    refactoreo del acton , llevandolo mas a lo que es struts2, ahora graba el usuario en la bd

--- trunk/ejemplo/src/actions/UsuarioAction.java
+++ trunk/ejemplo/src/actions/UsuarioAction.java
@@ -7,6 +7,7 @@ import javax.servlet.jsp.PageContext;
 import org.apache.struts2.interceptor.SessionAware;
 import usuarios.appl.UsuarioAppl;
 import usuarios.dto.PerfilDTO;
+import usuarios.dto.UsuarioDTO;
 import beans.PerfilesBean;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
@@ -19,51 +20,39 @@ public class UsuarioAction extends ActionSupport implements SessionAware {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Map<String,Object> session;
-	private String nombre;
-	private String apellido;
-	private String dni;
-	private String contrasena;
-    public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getDni() {
-		return dni;
-	}
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	public String getContrasena() {
-		return contrasena;
-	}
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
+	private UsuarioDTO user ;
+	private PerfilDTO perfil;
 	
+	public UsuarioDTO getUser() {
+		return user;
+	}
+	public void setUser(UsuarioDTO usuarioDTO) {
+		this.user = usuarioDTO;
+	}
+		
+	public String grabar(){
+		boolean error=false;
+		System.out.println(this.user.getApellido());
+		UsuarioAppl usuarioAppl = new UsuarioAppl();
+		PerfilDTO perfil1 = new PerfilDTO();
+		perfil1.setId(1);				
+		this.user.setPerfil(perfil1);
+		usuarioAppl.addUsuario(user);
+		if (error){
+		addActionError("El usuario que quiere ingresar ya es existente.");
+		return "error";
+		}
+		return "add";
+	}
 	@SuppressWarnings("unchecked")
 	public String execute() {
 		UsuarioAppl usuarioAppl = new UsuarioAppl();
-		PerfilesBean perfiles = new PerfilesBean();
+		PerfilesBean perfilesBean = new PerfilesBean();
 		List<PerfilDTO> listaPerfiles =  usuarioAppl.getPerfiles();
-		perfiles.setPerfiles(listaPerfiles);
-		
-		
-		
-		
+		perfilesBean.setPerfiles(listaPerfiles);	
 		Map session = ActionContext.getContext().getSession();
-        session.put("perfiles",perfiles);
-        this.setSession(session);
-         
-                       
-
+        session.put("perfilesBean",perfilesBean);
+        this.setSession(session);        
     	 return "success";
     }
 	public void setSession(Map<String, Object> arg0) {
