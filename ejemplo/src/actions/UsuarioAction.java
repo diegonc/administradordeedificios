5fc3453ce29a6f1af6e42d9395708678a8234722
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-22 07:09:08 +0000

    cargo un checkboxlist con una lista de String desde el action Agrego resultado de la actualizacion para redireccion al action correspondiente

--- ejemplo/src/actions/UsuarioAction.java
+++ ejemplo/src/actions/UsuarioAction.java
@@ -1,16 +1,24 @@
 package actions;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.jsp.PageContext;
 
 import org.apache.struts2.interceptor.SessionAware;
+import org.hibernate.SessionFactory;
+
 import usuarios.appl.UsuarioAppl;
 import usuarios.dto.PerfilDTO;
 import usuarios.dto.UsuarioDTO;
+import utilidades.HibernateUtil;
+import beans.EdificiosBean;
 import beans.PerfilesBean;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+
+import edificio.EdificioAppl;
+import edificio.EdificioDTO;
 
 
 public class UsuarioAction extends ActionSupport implements SessionAware {
@@ -19,10 +27,26 @@ public class UsuarioAction extends ActionSupport implements SessionAware {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	private Map<String,Object> session;
+	
 	private UsuarioDTO user ;
+	
 	private PerfilDTO perfil;
+	
 	private Integer id;
+	
+	private ArrayList<String> lista ;
+	
+	private String perfilesSeleccionados;
+	
+	public ArrayList<String> getLista() {
+		return lista;
+	}
+	public void setLista(ArrayList<String> lista) {
+		this.lista = lista;
+	}
+	
 	UsuarioAppl usuarioAppl = new UsuarioAppl();
 	
 	public Integer getId() {
@@ -31,6 +55,12 @@ public class UsuarioAction extends ActionSupport implements SessionAware {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	public String getPerfilesSeleccionados() {
+		return perfilesSeleccionados;
+	}
+	public void setPerfilesSeleccionados(String perfilesSeleccionados) {
+		this.perfilesSeleccionados = perfilesSeleccionados;
+	}
 	public UsuarioDTO getUser() {
 		return user;
 	}
@@ -38,7 +68,8 @@ public class UsuarioAction extends ActionSupport implements SessionAware {
 		this.user = usuarioDTO;
 	}
 	public String actualizar(){
-		return SUCCESS;
+		
+		return "actualizacion";
 	}
 	public String grabar(){
 		boolean error=false;
@@ -53,23 +84,36 @@ public class UsuarioAction extends ActionSupport implements SessionAware {
 		addActionError("El usuario que quiere ingresar ya es existente.");
 		return "error";
 		}
+		System.out.println(this.perfilesSeleccionados);
 		return "add";
 	}
 	
 	@SuppressWarnings("unchecked")
 	public String execute() {
+		 //recupero los edificios
+		EdificiosBean listaEdificios = new EdificiosBean();
+		EdificioAppl edifAppl = new EdificioAppl();
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		ArrayList<EdificioDTO> listaE = (ArrayList<EdificioDTO>) edifAppl.getAllEdificios(factory);
+		listaEdificios.setEdificios(listaE);
+		   
 		
-		PerfilesBean perfilesBean = new PerfilesBean();
 		List<PerfilDTO> listaPerfiles =  usuarioAppl.getPerfiles();
-		perfilesBean.setPerfiles(listaPerfiles);	
+			
 		Map session = ActionContext.getContext().getSession();
-        session.put("perfilesBean",perfilesBean);
-        this.setSession(session);       
-        
+	    session.put("edificios",listaEdificios);
+               
+        this.lista= new ArrayList<String>();
+        for (PerfilDTO p :listaPerfiles) {
+        	lista.add(p.getDescripcion());
+			
+		}
+
         if(this.id!=null){
         	user=usuarioAppl.getUsuario(this.id.intValue());
         	  session.put("user",user);
         }
+        this.setSession(session);
     	 return "success";
     	 
     }
