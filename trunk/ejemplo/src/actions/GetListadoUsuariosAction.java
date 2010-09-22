a93325c6ce4931a6bfa34e330ff3e7abf2e40dae
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-22 07:09:08 +0000

    cargo un checkboxlist con una lista de String desde el action Agrego resultado de la actualizacion para redireccion al action correspondiente

--- trunk/ejemplo/src/actions/GetListadoUsuariosAction.java
+++ trunk/ejemplo/src/actions/GetListadoUsuariosAction.java
@@ -2,19 +2,26 @@ package actions;
 
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
+import org.hibernate.SessionFactory;
 
 import usuarios.appl.UsuarioAppl;
 import usuarios.dto.PerfilDTO;
 import usuarios.dto.UsuarioDTO;
+import utilidades.HibernateUtil;
+import beans.EdificiosBean;
 import beans.PerfilesBean;
 import beans.UsuariosBean;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+
+import edificio.EdificioAppl;
+import edificio.EdificioDTO;
 
 public class GetListadoUsuariosAction extends ActionSupport implements SessionAware{
 
@@ -30,6 +37,27 @@ public class GetListadoUsuariosAction extends ActionSupport implements SessionAw
 	private UsuarioAppl userAppl = new UsuarioAppl();
 	
 	private UsuarioDTO user;
+	
+	private ArrayList<String> lista ;
+	
+	public ArrayList<String> getLista() {
+		return lista;
+	}
+
+	public void setLista(ArrayList<String> lista) {
+		this.lista = lista;
+	}
+
+	public String getPerfilesSeleccionados() {
+		return perfilesSeleccionados;
+	}
+
+	public void setPerfilesSeleccionados(String perfilesSeleccionados) {
+		this.perfilesSeleccionados = perfilesSeleccionados;
+	}
+
+	private String perfilesSeleccionados;
+	
 
 	public UsuarioDTO getUser() {
 		return user;
@@ -54,13 +82,26 @@ public class GetListadoUsuariosAction extends ActionSupport implements SessionAw
 	
 	
 	public String editar(){
+		 //recupero los edificios
+		EdificiosBean listaEdificios = new EdificiosBean();
+		EdificioAppl edifAppl = new EdificioAppl();
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		ArrayList<EdificioDTO> listaE = (ArrayList<EdificioDTO>) edifAppl.getAllEdificios(factory);
+		listaEdificios.setEdificios(listaE);
+		   
+		
 		if(this.id!=null)
 		this.user = userAppl.getUsuario(id.intValue());
-		PerfilesBean perfilesBean = new PerfilesBean();
+		
 		List<PerfilDTO> listaPerfiles =  userAppl.getPerfiles();
-		perfilesBean.setPerfiles(listaPerfiles);	
+        this.lista= new ArrayList<String>();
+        for (PerfilDTO p :listaPerfiles) {
+        	lista.add(p.getDescripcion());
+			
+		}
+
 		Map session = ActionContext.getContext().getSession();
-        session.put("perfilesBean",perfilesBean);
+	    session.put("edificios",listaEdificios);
         
 		UsuariosBean userEditar = new UsuariosBean();
 		userEditar.setUsuarioUnico(this.user);
