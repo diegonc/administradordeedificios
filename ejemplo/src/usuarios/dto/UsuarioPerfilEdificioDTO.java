6fd58d515588bfc940b9b163272e15bc85b3204b
dmpstaltari <dmpstaltari@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-30 04:19:11 +0000

    Se elimina el DTO de usuarioPerfil, se agrega el que mapea a la tabla USUARIO_PERFIL_EDIFICIO

--- ejemplo/src/usuarios/dto/UsuarioPerfilEdificioDTO.java
+++ ejemplo/src/usuarios/dto/UsuarioPerfilEdificioDTO.java
@@ -10,14 +10,17 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import edificio.EdificioDTO;
+
 @SuppressWarnings("serial")
 @Entity
-@Table (name="USUARIO_PERFIL")
-public class UsuarioPerfilDTO implements Serializable{
+@Table (name="USUARIO_PERFIL_EDIFICIO")
+public class UsuarioPerfilEdificioDTO implements Serializable{
 	
 	private int id;
 	private UsuarioDTO usuario;
 	private PerfilDTO perfil;
+	private EdificioDTO edificio;
 	
 	@Id
 	@GeneratedValue
@@ -46,6 +49,16 @@ public class UsuarioPerfilDTO implements Serializable{
 	public void setPerfil(PerfilDTO perfil) {
 		this.perfil = perfil;
 	}
+	
+	@ManyToOne
+	@JoinColumn(name="ID_EDIFICIO",nullable=true)
+	public EdificioDTO getEdificio() {
+		return edificio;
+	}
+	public void setEdificio(EdificioDTO edificio) {
+		this.edificio = edificio;
+	}
 		
+	
 
 }
