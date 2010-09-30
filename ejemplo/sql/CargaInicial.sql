acf839abe40de0ea1730764f28089e55a8bc469b
dmpstaltari <dmpstaltari@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-09-30 03:44:30 +0000

    modificacion de las tablas de usuario y script de carga inicial.

--- ejemplo/sql/CargaInicial.sql
+++ ejemplo/sql/CargaInicial.sql
@@ -1,7 +1,6 @@
 insert into `perfil`(`DESCRIPCION`,`ID`) values ('Administrador',1);
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Concejero de Administracion',2);
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Empleado',3);
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Presidente de consorcio',4);
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Encargado de Edificio',5);
-insert into `usuario`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`,`ID_EDIFICIO`,`ID`) values ('Nicolino','Roche','adminpass',30110082,'adminuser',null,1);
-insert into `usuario_perfil`(`ID_USUARIO`,`ID_PERFIL`,`ID`) values (1,1,1);
+insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Gastos',2);
+insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Cobros',3);
+insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Edificio',4);
+insert into `usuario`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`,`ID`) values ('Nicolino','Roche','adminpass',30110082,'adminuser',1);
+insert into `usuario_perfil_edificio`(`ID_USUARIO`,`ID_PERFIL`,`ID`,`ID_EDIFICIO`) values (1,1,1,null);
