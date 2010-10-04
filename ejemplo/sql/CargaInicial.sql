0f568e7aa9d6e23111886253aeea7c65981929eb
dmpstaltari <dmpstaltari@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-10-04 00:41:47 +0000

    modificacion de carga inicial

--- ejemplo/sql/CargaInicial.sql
+++ ejemplo/sql/CargaInicial.sql
@@ -2,5 +2,5 @@ insert into `perfil`(`DESCRIPCION`,`ID`) values ('Administrador',1);
 insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Gastos',2);
 insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Cobros',3);
 insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Edificio',4);
-insert into `usuario`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`,`ID`) values ('Nicolino','Roche','adminpass',30110082,'adminuser',1);
-insert into `usuario_perfil_edificio`(`ID_USUARIO`,`ID_PERFIL`,`ID`,`ID_EDIFICIO`) values (1,1,1,null);
+insert into `usuario`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`,`ID`) values ('Nicolino','Roche','adminpass',1,'adminuser',1);
+insert into `usuario_perfil`(`ID_USUARIO`,`ID_PERFIL`,`ID`) values (1,1,1);
