404252b711ff9973fa1c794ade53b02a23438912
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-10-04 02:40:30 +0000

    Actions para la vinculacion de perfiles. TODO: unir con la persistencia que hizo Dario.

---
+++ ejemplo/src/actions/GetListadoVinculacionAction.java
@@ -0,0 +1,47 @@
+package actions;
+
+import java.util.List;
+import java.util.Map;
+
+import usuarios.appl.UsuarioAppl;
+import usuarios.dto.UsuarioDTO;
+import beans.UsuariosBean;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GetListadoVinculacionAction extends ActionSupport {
+	
+	private UsuarioAppl userAppl = new UsuarioAppl();
+	
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	private Map<String, Object> session;
+	
+	
+	public String execute() {
+			
+			
+			List<UsuarioDTO> listaUsuario = userAppl.getUsuarios() ;
+			UsuariosBean listado = new UsuariosBean();
+			listado.setUsers(listaUsuario);		
+			Map<String,Object> session = ActionContext.getContext().getSession();
+	        session.put("listado",listado);
+	        setSession(session);
+	              
+	    	 return "listar";
+	    }
+	
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		this.session = arg0;
+	}
+
+}
