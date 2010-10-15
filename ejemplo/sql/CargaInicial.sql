40e5349c581734ecad5d1b2f8e35914a9ae41886
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-10-15 04:34:24 +0000

    saco la columna ID  del insert que es autoincrementada

--- ejemplo/sql/CargaInicial.sql
+++ ejemplo/sql/CargaInicial.sql
@@ -2,5 +2,5 @@ insert into `perfil`(`DESCRIPCION`,`ID`) values ('Administrador',1);
 insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Gastos',2);
 insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Cobros',3);
 insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Edificio',4);
-insert into `usuario`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`,`ID`) values ('Nicolino','Roche','adminpass',1,'adminuser',1);
-insert into `usuario_perfil`(`ID_USUARIO`,`ID_PERFIL`,`ID`) values (1,1,1);
+insert into `usuario`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`) values ('Nicolino','Roche','adminpass',1,'adminuser');
+insert into `usuario_perfil`(`ID_USUARIO`,`ID_PERFIL`) values (1,1);
