b74e4926e74b6ae1407a64b044b18b2c62494f10
dnietoc <dnietoc@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-10-17 00:40:41 +0000

    daaa mayusculas

--- ejemplo/sql/CargaInicial.sql
+++ ejemplo/sql/CargaInicial.sql
@@ -1,6 +1,6 @@
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Administrador',1);
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Gastos',2);
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Cobros',3);
-insert into `perfil`(`DESCRIPCION`,`ID`) values ('Responsable de Edificio',4);
-insert into `usuario`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`) values ('Nicolino','Roche','adminpass',1,'adminuser');
-insert into `usuario_perfil`(`ID_USUARIO`,`ID_PERFIL`) values (1,1);
+insert into `PERFIL`(`DESCRIPCION`,`ID`) values ('Administrador',1);
+insert into `PERFIL`(`DESCRIPCION`,`ID`) values ('Responsable de Gastos',2);
+insert into `PERFIL`(`DESCRIPCION`,`ID`) values ('Responsable de Cobros',3);
+insert into `PERFIL`(`DESCRIPCION`,`ID`) values ('Responsable de Edificio',4);
+insert into `USUARIO`(`NOMBRE`,`APELLIDO`,`PASSWORD`,`DNI`,`USUARIO`) values ('Nicolino','Roche','adminpass',1,'adminuser');
+insert into `USUARIO_PERFIL`(`ID_USUARIO`,`ID_PERFIL`) values (1,1);
