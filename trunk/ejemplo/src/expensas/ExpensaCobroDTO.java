9647551f3639c06496f8e9cc481acecec6a23ff1
dnietoc <dnietoc@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-10-18 02:17:24 +0000

    ExpensaCobroDTO: attributos consolidado y responsableCobro.

--- trunk/ejemplo/src/expensas/ExpensaCobroDTO.java
+++ trunk/ejemplo/src/expensas/ExpensaCobroDTO.java
@@ -4,8 +4,13 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+
+import usuarios.dto.UsuarioDTO;
 
 @SuppressWarnings("serial")
 @Entity
@@ -15,6 +20,8 @@ public class ExpensaCobroDTO extends ExpensaDTO{
 
 	private Date fecha;
 	private String comprobante;
+	private boolean consolidado = false;
+	private UsuarioDTO responsableCobro;
 	
 	@Column(name="FECHA",nullable=false)
 	public Date getFecha() {
@@ -31,5 +38,22 @@ public class ExpensaCobroDTO extends ExpensaDTO{
 	public void setComprobante(String comprobante) {
 		this.comprobante = comprobante;
 	}
-		
+
+	@Column(name="CONSOLIDADO",nullable=false)
+	public boolean getConsolidado() {
+		return consolidado;
+	}
+	public void setConsolidado(boolean consolidado) {
+		this.consolidado = consolidado;
+	}
+
+	@ManyToOne(fetch=FetchType.LAZY, optional=false)
+	@JoinColumn(name="RESPONSABLECOBRO_ID", updatable=false)
+	public UsuarioDTO getResponsableCobro() {
+		return responsableCobro;
+	}
+	public void setResponsableCobro(UsuarioDTO usuario) {
+		responsableCobro = usuario;
+	}
+
 }
