99b0a8d579f5185f8b639868ec2b41ac31426f8e
tanomartin05@gmail.com <tanomartin05@gmail.com@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-10-22 22:48:43 +0000

    Registro de cobros de expensas punto (5) Consulta de liquidaciones para informar montos a pagar (9)
    para ver... cuando se registra un cobro, ya el monto que pago o recien cuando se consolida??

---
+++ trunk/ejemplo/src/expensas/ExpensaAppl.java
@@ -0,0 +1,32 @@
+package expensas;
+
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class ExpensaAppl {
+	private static Logger _logger = Logger.getLogger(ExpensaAppl.class);
+	
+	public SessionFactory createSessionFactory() {
+		AnnotationConfiguration configuration = new AnnotationConfiguration();
+		configuration.configure("/hibernate.cfg.xml");
+		return configuration.buildSessionFactory();
+	}
+	
+	public List<ExpensaDTO> getAllExpensaPorIdProp(SessionFactory factory, int propId) {
+		Session session = factory.openSession();
+		try {
+			Query query = session
+			.createQuery("select u from ExpensaDTO u where u.id=:idprop");
+			query.setInteger("idprop", propId);
+			List<ExpensaDTO> results = query.list();
+			return results; 
+		} finally {
+			session.close();
+		}
+	}
+	
+}
