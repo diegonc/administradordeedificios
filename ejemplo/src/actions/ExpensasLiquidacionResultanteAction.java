417d060f153dccf032de23e5bd031c4311682928
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-10-31 23:45:08 +0000

    control de seleccion de periodo, para no liquidar dos veces el mismo periodo

--- ejemplo/src/actions/ExpensasLiquidacionResultanteAction.java
+++ ejemplo/src/actions/ExpensasLiquidacionResultanteAction.java
@@ -25,6 +25,8 @@ import gastos.dto.TipoGastoDTO;
 public class ExpensasLiquidacionResultanteAction extends ActionSupport {
 	
 	private int id;
+	private int mes;
+	private int anio;
 	@SuppressWarnings("unused")
 	private Map<String, Object> session;
 	private EdificioAppl edifAppl = new EdificioAppl();
@@ -39,27 +41,49 @@ public class ExpensasLiquidacionResultanteAction extends ActionSupport {
 		this.id = id;
 	}
 
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
 	public String execute() {
 			
-		HashMap<TipoGastoDTO, List<GastoDTO>> gastosLiquidacion = null;
-		SessionFactory factory = HibernateUtil.getSessionFactory();
-		EdificioDTO edificio = edifAppl.getEdificio(factory, id);
-		LiquidacionBean expensaDetalle = new LiquidacionBean();
-		Map<String, Object> session = ActionContext.getContext().getSession();
-		session.put("edificio",edificio);
-		//Si es fijo no paso los gastos
-		if (edificio.getForma_liq_exp().equals("PRORRATEO")){
-			gastosLiquidacion= expensasCalculoAppl.obtenerGastosPorEdificioAgrupadoPorTipo(id);
-			expensaDetalle.setGastosDelPeriodo(gastosLiquidacion);		
-			
+		//TODO: ver si la liquidacion es del mes
+		if (mes==1 && anio==2005){
+			addActionError("Ese mes se encuentra liquidado.");
+			return "error";
 		}else{
-			List<ExpensaDTO> expensasFijas = expensasFijasAppl.obtenerExpensasFijas(id);
-			expensaDetalle.setExpensas(expensasFijas);
+			HashMap<TipoGastoDTO, List<GastoDTO>> gastosLiquidacion = null;
+			SessionFactory factory = HibernateUtil.getSessionFactory();
+			EdificioDTO edificio = edifAppl.getEdificio(factory, id);
+			LiquidacionBean expensaDetalle = new LiquidacionBean();
+			Map<String, Object> session = ActionContext.getContext().getSession();
+			session.put("edificio",edificio);
+			//Si es fijo no paso los gastos
+			if (edificio.getForma_liq_exp().equals("PRORRATEO")){
+				gastosLiquidacion= expensasCalculoAppl.obtenerGastosPorEdificioAgrupadoPorTipo(id);
+				expensaDetalle.setGastosDelPeriodo(gastosLiquidacion);		
+				
+			}else{
+				List<ExpensaDTO> expensasFijas = expensasFijasAppl.obtenerExpensasFijas(id);
+				expensaDetalle.setExpensas(expensasFijas);
+				
+			}
 			
+			session.put("detalleExpensa",expensaDetalle);		
+	        this.setSession(session);
 		}
-		
-		session.put("detalleExpensa",expensaDetalle);		
-        this.setSession(session);
 		return "success";		
 		
 	}
