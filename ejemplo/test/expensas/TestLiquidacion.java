33d9ef693afe93f9420a4387cf86202987fd9f46
dmpstaltari <dmpstaltari@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-11-05 04:04:54 +0000

    Refactoring de appl de expensas

--- ejemplo/test/expensas/TestLiquidacion.java
+++ ejemplo/test/expensas/TestLiquidacion.java
@@ -5,12 +5,9 @@ import java.util.Iterator;
 import java.util.List;
 
 import junit.framework.TestCase;
-
 import propiedades.PropiedadDTO;
-
 import utilidades.Periodo;
-import expensas.appl.ExpensaPrevisionAppl;
-import expensas.calculo.ElementoPrevisionGasto;
+import expensas.appl.liquidacion.ExpensaProrrateoPrevisionAppl;
 import expensas.dto.ExpensaDTO;
 import gastos.dto.GastoDTO;
 import gastos.dto.TipoGastoDTO;
@@ -18,87 +15,66 @@ import gastos.dto.TipoGastoDTO;
 public class TestLiquidacion extends TestCase{
 
 	public void testLiquidar(){
+		int idEdificio = 1;
 		Periodo periodo = new Periodo(11,2010);
-		ExpensaPrevisionAppl expensaPrevisionAppl = new ExpensaPrevisionAppl();
+		ExpensaProrrateoPrevisionAppl expensaProrrateoPrevisionAppl = new ExpensaProrrateoPrevisionAppl();
 		
-		HashMap<TipoGastoDTO, ElementoPrevisionGasto> tipoGastoElementoPrevisionGasto = expensaPrevisionAppl.obtenerGastosPorEdificioYPeriodoAgrupadoPorTipo(1, periodo, ExpensaDTO.tipoOrdinario);
+		HashMap<TipoGastoDTO, List<GastoDTO>> tipoGastoGastos = expensaProrrateoPrevisionAppl.obtenerGastosPorEdificioYPeriodoAgrupadoPorTipo(idEdificio, periodo, ExpensaDTO.tipoOrdinario);
 		
-		Iterator<TipoGastoDTO> it = tipoGastoElementoPrevisionGasto.keySet().iterator();
+		Iterator<TipoGastoDTO> it = tipoGastoGastos.keySet().iterator();
 	    while (it.hasNext()) {
-	        TipoGastoDTO tipoGasto = (TipoGastoDTO) it.next();
-	        ElementoPrevisionGasto elementoPrevisionGasto = (ElementoPrevisionGasto) tipoGastoElementoPrevisionGasto.get(tipoGasto);
+	        double montoAcumulado =0;
+	    	TipoGastoDTO tipoGasto = (TipoGastoDTO) it.next();
+	        List<GastoDTO> gastos = (List<GastoDTO>) tipoGastoGastos.get(tipoGasto);
 	        System.out.println(tipoGasto.getCodigo());
-	        
-	        System.out.println("******Previsiones mes anterior******");
-	        for (GastoDTO gastoActual : elementoPrevisionGasto.getPrevisionesMesAnterior()) {
-	        	System.out.println(gastoActual.getDetalle()+" : "+gastoActual.getMonto());
-			}
-	        System.out.println("Total previsiones mes anterior: " + elementoPrevisionGasto.obtenerMontoPrevisionMesAnterior());
-	        	        
-	        System.out.println("******Gastos mes anterior******");
-	        for (GastoDTO gastoActual : elementoPrevisionGasto.getGastosRealesMesAnterior()) {
-	        	System.out.println(gastoActual.getDetalle()+" : "+gastoActual.getMonto());
-			}
-	        System.out.println("Total gastos reales mes anterior: " + elementoPrevisionGasto.obtenerMontoGastosRealesMesAnterior());
-	        System.out.println("Total diferencia prevision gasto mes anterior: " + elementoPrevisionGasto.obtenerMontoDiferenciaPrevisionGastoMesAnterior());
-	        
 	        System.out.println("******Previsiones mes actual******");
-	        for (GastoDTO gastoActual : elementoPrevisionGasto.getPrevisiones()) {
+	        for (GastoDTO gastoActual : gastos) {
 	        	System.out.println(gastoActual.getDetalle()+" : "+gastoActual.getMonto());
+	        	montoAcumulado+=gastoActual.getMonto();
 			}
-	        System.out.println("Total previsiones mes actual: " + elementoPrevisionGasto.obtenerMontoPrevisionMesActual());
-	        System.out.println("Totalizacion Tipo Gasto : " + elementoPrevisionGasto.obtenerMontoTotal());
-	        System.out.println(""); 
+	        System.out.println("Totalizacion Tipo Gasto : " + montoAcumulado);
 	    }
 	    
-	    List<ExpensaDTO> expensas = expensaPrevisionAppl.obtenerExpensasPorTipoPorEdificioYPeriodo(tipoGastoElementoPrevisionGasto, 1, ExpensaDTO.tipoOrdinario);
-	    System.out.println("Expensas Por Propiedad");
+	    List<ExpensaDTO> expensas = expensaProrrateoPrevisionAppl. obtenerExpensasPorTipoPorEdificioYPeriodo(tipoGastoGastos,idEdificio, ExpensaDTO.tipoOrdinario);
+	    
+	    System.out.println("\nExpensas Por Propiedad");
+	    System.out.println("***********************ORDINARIOS**************************");
 		
 		PropiedadDTO propiedadActual;
 		for (ExpensaDTO expensa: expensas) {
 			propiedadActual = expensa.getPropiedad();
 			System.out.println("Piso-Depto: " + propiedadActual.getNivel()+" - "+propiedadActual.getOrden()+" = "+expensa.getMonto());
 		}
-		System.out.println();
-		System.out.println("***********************EXTAORDINARIOS**************************");
-		tipoGastoElementoPrevisionGasto = expensaPrevisionAppl.obtenerGastosPorEdificioYPeriodoAgrupadoPorTipo(1, periodo, ExpensaDTO.tipoExtraordinario);
 		
-		it = tipoGastoElementoPrevisionGasto.keySet().iterator();
-	    while (it.hasNext()) {
-	        TipoGastoDTO tipoGasto = (TipoGastoDTO) it.next();
-	        ElementoPrevisionGasto elementoPrevisionGasto = (ElementoPrevisionGasto) tipoGastoElementoPrevisionGasto.get(tipoGasto);
+		/***************************************************************************************************************************/
+		
+		/*
+		tipoGastoGastos = expensaProrrateoPrevisionAppl.obtenerGastosPorEdificioYPeriodoAgrupadoPorTipo(idEdificio, periodo, ExpensaDTO.tipoExtraordinario);
+		it = tipoGastoGastos.keySet().iterator();
+		System.out.println();
+		System.out.println("***********************EXTRAORDINARIOS**************************");
+		while (it.hasNext()) {
+	    	double montoAcumulado =0;
+	    	TipoGastoDTO tipoGasto = (TipoGastoDTO) it.next();
+	        List<GastoDTO> gastos = (List<GastoDTO>) tipoGastoGastos.get(tipoGasto);
 	        System.out.println(tipoGasto.getCodigo());
-	        
-	        System.out.println("******Previsiones mes anterior******");
-	        for (GastoDTO gastoActual : elementoPrevisionGasto.getPrevisionesMesAnterior()) {
-	        	System.out.println(gastoActual.getDetalle()+" : "+gastoActual.getMonto());
-			}
-	        System.out.println("Total previsiones mes anterior: " + elementoPrevisionGasto.obtenerMontoPrevisionMesAnterior());
-	        	        
-	        System.out.println("******Gastos mes anterior******");
-	        for (GastoDTO gastoActual : elementoPrevisionGasto.getGastosRealesMesAnterior()) {
-	        	System.out.println(gastoActual.getDetalle()+" : "+gastoActual.getMonto());
-			}
-	        System.out.println("Total gastos reales mes anterior: " + elementoPrevisionGasto.obtenerMontoGastosRealesMesAnterior());
-	        System.out.println("Total diferencia prevision gasto mes anterior: " + elementoPrevisionGasto.obtenerMontoDiferenciaPrevisionGastoMesAnterior());
-	        
 	        System.out.println("******Previsiones mes actual******");
-	        for (GastoDTO gastoActual : elementoPrevisionGasto.getPrevisiones()) {
+	        for (GastoDTO gastoActual : gastos) {
 	        	System.out.println(gastoActual.getDetalle()+" : "+gastoActual.getMonto());
+	        	montoAcumulado+=gastoActual.getMonto();
 			}
-	        System.out.println("Total previsiones mes actual: " + elementoPrevisionGasto.obtenerMontoPrevisionMesActual());
-	        System.out.println("Totalizacion Tipo Gasto : " + elementoPrevisionGasto.obtenerMontoTotal());
-	        System.out.println(""); 
+	        System.out.println("Totalizacion Tipo Gasto : " + montoAcumulado);
 	    }
+		System.out.println("*****************************************************");
 	    
-	    expensas = expensaPrevisionAppl.obtenerExpensasPorTipoPorEdificioYPeriodo(tipoGastoElementoPrevisionGasto, 1, ExpensaDTO.tipoExtraordinario);
+	    expensas = expensaProrrateoPrevisionAppl.obtenerExpensasPorTipoPorEdificioYPeriodo(tipoGastoGastos, idEdificio, ExpensaDTO.tipoExtraordinario);
 	    System.out.println("Expensas Por Propiedad");
 				
 		for (ExpensaDTO expensa: expensas) {
 			propiedadActual = expensa.getPropiedad();
 			System.out.println("Piso-Depto: " + propiedadActual.getNivel()+" - "+propiedadActual.getOrden()+" = "+expensa.getMonto());
 		}
-
+		 */
 		
 	}
 }
