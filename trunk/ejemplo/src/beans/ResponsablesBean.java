ea8fd48cc7c4b81d757d8628af33e1cd32ab1421
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-11-17 04:58:14 +0000

    para listar los responsables para luego mostrar el historial

---
+++ trunk/ejemplo/src/beans/ResponsablesBean.java
@@ -0,0 +1,19 @@
+package beans;
+
+import java.util.List;
+
+import propiedades.Responsable;
+
+public class ResponsablesBean {
+	
+	List<Responsable> responsables ;
+
+	public List<Responsable> getResponsables() {
+		return responsables;
+	}
+
+	public void setResponsables(List<Responsable> responsables) {
+		this.responsables = responsables;
+	}
+
+}
