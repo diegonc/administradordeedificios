f80f0cc60cc833df6ece34b6efce15fab5445c0d
adrianachelotti <adrianachelotti@0cfc2dd9-e269-09ce-bb95-ef3fcade85ae>
2010-12-02 07:15:46 +0000

    agrego al bean para pasar la lista de dnis

--- ejemplo/src/beans/ResponsablesBean.java
+++ ejemplo/src/beans/ResponsablesBean.java
@@ -6,6 +6,16 @@ import propiedades.Responsable;
 
 public class ResponsablesBean {
 	
+	List<Integer> listaDNIs;
+	
+	public List<Integer> getListaDNIs() {
+		return listaDNIs;
+	}
+
+	public void setListaDNIs(List<Integer> listaDNIs) {
+		this.listaDNIs = listaDNIs;
+	}
+
 	List<Responsable> responsables ;
 
 	public List<Responsable> getResponsables() {
